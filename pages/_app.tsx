f48e235f5f53238f097b0fc7cdf80558a60079bd
spuf <556061+spuf@users.noreply.github.com>
2022-05-30 12:55:43 +0300

    style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
+import 'sanitize.css'
+import 'sanitize.css/forms.css'
+import 'sanitize.css/typography.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
