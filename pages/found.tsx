81dd2940d2e23ec73080d85024b43b957737bcc5
Arseny Razin <arseny@razin.net>
2023-01-26 18:19:55 +0700

    fresh

---
+++ pages/found.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Found: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <meta name="robots" content="noindex" />
+        <title>Found</title>
+      </Head>
+      <div style={{margin: '1rem'}}>
+        <a href="mailto:arseny@razin.net">arseny@razin.net</a>
+      </div>
+    </div>
+  )
+}
+
+export default Found
