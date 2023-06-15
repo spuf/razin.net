77df390e9811283bdc31d766d0849a74aaf06464
spuf <556061+spuf@users.noreply.github.com>
2023-06-15 12:26:08 +0300

    text3d

--- app/3d/client.tsx
+++ app/3d/client.tsx
@@ -1,23 +1,20 @@
 'use client'
 
-import { Suspense, useImperativeHandle, useRef, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { Canvas } from '@react-three/fiber'
-import { useCursor, MeshDistortMaterial, Preload, PerspectiveCamera, View } from '@react-three/drei'
-import tunnel from 'tunnel-rat'
+import { Suspense, useImperativeHandle, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Preload, PerspectiveCamera, View, Text3D, Center, MeshDistortMaterial } from '@react-three/drei'
+import tunnelRat from 'tunnel-rat'
+
+const tunnel = tunnelRat()
 
 export default function Client() {
-  const r3f = tunnel()
-  const ref = useRef<HTMLElement | HTMLDivElement>(null)
-  const router = useRouter()
-  const [hovered, hover] = useState(false)
-  useCursor(hovered)
-  const localRef = useRef<HTMLElement>(null)
-  useImperativeHandle<HTMLElement | null, HTMLElement | null>(ref, () => localRef.current)
+  const wrapperRef = useRef<HTMLElement>(null)
+  const containerRef = useRef<HTMLElement>(null)
+  useImperativeHandle<HTMLElement | null, HTMLElement | null>(wrapperRef, () => containerRef.current)
 
   return (
     <div
-      ref={ref as React.RefObject<HTMLDivElement>}
+      ref={wrapperRef as React.RefObject<HTMLDivElement>}
       style={{
         position: 'relative',
         width: '100%',
@@ -26,28 +23,16 @@ export default function Client() {
         touchAction: 'auto',
       }}
     >
-      <>
-        <div
-          ref={localRef as React.RefObject<HTMLDivElement>}
-          style={{
-            height: '100vh',
-          }}
-        />
-        <r3f.In>
-          <View track={localRef as React.MutableRefObject<HTMLElement>}>
-            <mesh onClick={() => router.push('/')} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)}>
-              <sphereGeometry args={[1, 64, 64]} />
-              <MeshDistortMaterial roughness={0.5} color={hovered ? 'hotpink' : '#1fb2f5'} />
-            </mesh>
-            <Suspense fallback={null}>
-              <ambientLight intensity={0.5} />
-              <pointLight position={[20, 30, 10]} intensity={1} />
-              <pointLight position={[-10, -10, -10]} color="blue" />
-              <PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
-            </Suspense>
-          </View>
-        </r3f.In>
-      </>
+      <div
+        ref={containerRef as React.RefObject<HTMLDivElement>}
+        style={{
+          height: '100vh',
+        }}
+      >
+        <tunnel.In>
+          <ClientView containerRef={containerRef} />
+        </tunnel.In>
+      </div>
       <Canvas
         style={{
           position: 'fixed',
@@ -57,12 +42,47 @@ export default function Client() {
           height: '100vh',
           pointerEvents: 'none',
         }}
-        eventSource={ref as React.MutableRefObject<HTMLElement>}
+        eventSource={wrapperRef as any}
         eventPrefix="client"
       >
-        <r3f.Out />
-        <Preload all />
+        <tunnel.Out />
       </Canvas>
     </div>
+  )
+}
+
+function ClientView({ containerRef }: { containerRef: React.RefObject<HTMLElement> }) {
+  const meshRef: any = useRef<HTMLElement>(null)
+  useFrame(({ clock }) => {
+    meshRef.current.rotation.y = Math.sin(clock.getElapsedTime()) / 10
+  })
+
+  return (
+    <View track={containerRef as any}>
+      <mesh ref={meshRef as any}>
+        <Center>
+          <Text3D
+            font={'/helvetiker_regular.typeface.json'}
+            size={0.5}
+            height={0.2}
+            curveSegments={10}
+            bevelEnabled
+            bevelThickness={0.01}
+            bevelSize={0.02}
+            bevelSegments={10}
+          >
+            Arseny Razin
+            <MeshDistortMaterial color={'deepskyblue'} />
+          </Text3D>
+        </Center>
+      </mesh>
+
+      <Suspense fallback={null}>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[20, 30, 10]} intensity={1} />
+        <pointLight position={[-10, -10, -10]} color="blue" />
+        <PerspectiveCamera makeDefault fov={40} position={[0, 0, 6]} />
+      </Suspense>
+    </View>
   )
 }
