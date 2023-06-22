4be58cf36265adfcf28bf9b0f99e3d38c0a4cde6
spuf <556061+spuf@users.noreply.github.com>
2023-06-22 16:38:20 +0300

    3d

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
+import Client from './client'
+import { Suspense } from 'react'
 import styles from './page.module.css'
 
 export const metadata = {
   title: 'Arseny Razin',
 }
-
 export default function Index() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Arseny Razin</h1>
-      </main>
-    </div>
+    <Suspense
+      fallback={
+        <div className={styles.container}>
+          <main className={styles.main}>
+            <h1 className={styles.title}>Arseny Razin</h1>
+          </main>
+        </div>
+      }
+    >
+      <Client />
+    </Suspense>
   )
 }
